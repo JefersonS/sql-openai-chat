c1f1cc59dafa6498c7975d02c288630d1fb58951
Jeferson Euclides <jefersonebs@gmail.com>
2024-11-13 23:30:02 -0300

    using tailwind + improving code

--- src/app/ChatScreen.tsx
+++ src/app/ChatScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./ChatScreen.css";
-import { remark } from 'remark';
-import html from 'remark-html';
+import { remark } from "remark";
+import html from "remark-html";
+import LoadingIcon from "./icons/LoadingIcon";
 
 type ChatScreenProps = {
   setTables: (tables: string[]) => void;
@@ -9,9 +9,13 @@ type ChatScreenProps = {
 
 const ChatScreen = ({ setTables }: ChatScreenProps) => {
   const [messages, setMessages] = useState([
-    { id: 1, role: "assistant", content: `Hi there! I'm your SQL Assistant, ready to make your data talk! To get started, please share your database connection string. Need an example? Here's a quick one: <br><br>
-    postgres://read_only_user:123456@autorack.proxy.rlwy.net:33527/railway <br><br>
-    Once connected, we'll dive straight into querying magic together!` },
+    {
+      id: 1,
+      role: "assistant",
+      content: `Hi there! I'm your SQL Assistant, ready to make your data talk! To get started, please share your database connection string. Need an example? Here's a quick one: <br><br>
+      postgres://read_only_user:123456@autorack.proxy.rlwy.net:33527/railway <br><br>
+      Once connected, we'll dive straight into querying magic together!`,
+    },
   ]);
   const [inputText, setInputText] = useState("");
   const [disabledSendButton, setDisabledSendButton] = useState(false);
@@ -37,19 +41,17 @@ const ChatScreen = ({ setTables }: ChatScreenProps) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ message: inputText.trim(), history: messages }),
-      })
-      const data = await res.json()
-      console.log({ data })
+      });
+      const data = await res.json();
       const message = data.message;
 
       if (data.model) {
-        const uniqueTables = data.model.map((v: { table_name: string }) => v.table_name).filter((v: string, i: number, a: string[]) => a.indexOf(v) === i);
-        console.log({ uniqueTables })
+        const uniqueTables = data.model
+          .map((v: { table_name: string }) => v.table_name)
+          .filter((v: string, i: number, a: string[]) => a.indexOf(v) === i);
         setTables(uniqueTables);
       }
 
-      console.log({ message })
-      console.log({ transformed: await transformFromMarkdown(message) })
       const assistantMessage = {
         id: messages.length + 2,
         role: "assistant",
@@ -63,75 +65,84 @@ const ChatScreen = ({ setTables }: ChatScreenProps) => {
         role: "assistant",
         content: "Sorry, something went wrong. Please try again.",
       };
-
       setMessages((prevMessages) => [...prevMessages, assistantMessage]);
     }
 
     setDisabledSendButton(false);
   };
 
-  const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
 
-  const handleKeyPress = (e: { key: string; }) => {
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       handleSendMessage();
     }
   };
 
   const transformFromMarkdown = async (content: string) => {
-    // first check if the string is already in html format
     const htmlRegex = /<[^>]*>/g;
     if (htmlRegex.test(content)) {
       return content;
     }
-    const processedContent = await remark()
-      .use(html)
-      .process(content);
+    const processedContent = await remark().use(html).process(content);
     return processedContent.toString();
-  }
+  };
 
-  const messagesEndRef = useRef<null | HTMLDivElement>(null)
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   useEffect(() => {
-    scrollToBottom()
+    scrollToBottom();
   }, [messages]);
 
   return (
-    <div className="chat-container">
-      <div className="messages-container">
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-white">
+      <div className="h-[70vh] w-full overflow-y-auto p-5 flex-grow">
         {messages.map((message) => (
-          <div key={message.id} className={`message ${message.role}`}>
-            <div className="message-content" dangerouslySetInnerHTML={{ __html: message.content }}>
-            </div>
+          <div
+            key={message.id}
+            className={`p-4 rounded-lg break-words my-3 mx-[20%] ${message.role === "user"
+              ? "bg-gray-200 self-end ml-auto max-w-[40%]"
+              : "bg-white text-black self-start mr-auto max-w-[60%]"
+              }`}
+          >
+            <div
+              dangerouslySetInnerHTML={{ __html: message.content }}
+            ></div>
           </div>
         ))}
         <div ref={messagesEndRef} />
       </div>
-      <div role="status" className="loading-spinner" hidden={!disabledSendButton}>
-        <svg aria-hidden="true" className="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-        </svg>
+      <div role="status" hidden={!disabledSendButton}>
+        <LoadingIcon />
         <span className="sr-only">Loading...</span>
       </div>
-      <div className="input-container">
+      <div className="w-full flex justify-between items-center p-2">
         <input
           type="text"
           placeholder="Type a message..."
           value={inputText}
           onChange={handleInputChange}
           onKeyDown={handleKeyPress}
+          className="w-[85%] ml-[20%] p-2 border border-gray-300 rounded-full text-base"
         />
-        <button onClick={handleSendMessage} disabled={disabledSendButton}>Send</button>
+        <button
+          onClick={handleSendMessage}
+          disabled={disabledSendButton}
+          className={`w-[12%] p-2 mr-[20%] rounded-full ${disabledSendButton
+            ? "bg-gray-300 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-400 text-white"
+            }`}
+        >
+          Send
+        </button>
       </div>
     </div>
-
   );
 };
 
